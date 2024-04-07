b85dca48f0b68c12eb76dab1f337c617d14769e5
lintangpa <lintang2505@gmail.com>
2024-04-07 18:07:28 +0700

    rename dragndrop2 to simplepresent1

--- app/Http/Controllers/SimplePresentController.php
+++ app/Http/Controllers/SimplePresentController.php
@@ -39,22 +39,22 @@ class SimplePresentController extends Controller
                 $notifications[] = $achievement->nama;
             }
         }
-        return view('user.simplepresent', compact('notifications', 'userProgress'));
+        return view('user.simplepresenthome', compact('notifications', 'userProgress'));
     }
 
     public function quest1()
     {
-        return view('user.dragndrop2');
+        return view('user.simplepresent1');
     }
 
     public function quest2()
     {
-        return view('user.dragndrop');
+        return view('user.simplepresent2');
     }
 
     public function quest3()
     {
-        return view('user.dragndrop2');
+        return view('user.simplepresent3');
     }
 
     private function storeAchievement(User $user, $achievementName)
